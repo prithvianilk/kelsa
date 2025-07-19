1112f5d604d146aa70dcb28ed55223fb30dc6845
prithvianilk <thepirateking3d2y@gmail.com>
2025-07-19 02:33:57 +0200

    Mount all service's data onto actual dir

--- desktop-recorder/main.py
+++ desktop-recorder/main.py
@@ -5,15 +5,16 @@ from kafka import KafkaProducer
 import time
 from application_details_fetcher import JxaApplicationDetailsFetcher, AppleScriptApplicationDetailsFetcher
 from work_recorder import KafkaWorkRecorder, FirstSuccessfulFetcherWorkRecorder
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from common.logger import ConsoleLogger
+from common.logger import get_customised_logger, LogLevel
+logger = get_customised_logger(LogLevel.DEBUG)
 
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9094'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 fetchers = [JxaApplicationDetailsFetcher(), AppleScriptApplicationDetailsFetcher()]
-logger = ConsoleLogger()
 recorder = FirstSuccessfulFetcherWorkRecorder(fetchers, logger, KafkaWorkRecorder(None, logger, producer, 'work-topic'))
 
 while True:
---
+++ pinot/bin/clean-everything.sh
@@ -0,0 +1,7 @@
+rm -rf data/kafka/config
+rm -rf data/kafka/data
+rm -rf data/controller/*
+rm -rf data/broker/*
+rm -rf data/server/*
+rm -rf data/zookeeper/data/*
+rm -rf data/zookeeper/datalog/*
