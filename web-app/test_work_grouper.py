8cb5203e4e46ae8335531964f9b775f134c8c53c
prithvianilk <thepirateking3d2y@gmail.com>
2025-07-30 23:31:52 +0530

    Add idea (intellij) work grouper

--- web-app/test_work_grouper.py
+++ web-app/test_work_grouper.py
@@ -1,4 +1,4 @@
-from work_grouper import ArcProjectNameApplicationWorkGrouper
+from work_grouper import ArcProjectNameApplicationWorkGrouper, IdeaProjectNameApplicationWorkGrouper
 
 def test_arc_project_name_application_work_grouper():
     arc_project_name_application_work_grouper = ArcProjectNameApplicationWorkGrouper()
@@ -16,3 +16,13 @@ def test_arc_project_name_application_work_grouper():
     ]
     assert expected_regrouped_work == arc_project_name_application_work_grouper.regroup_work_by_tab(work)
 
+def test_idea_project_name_application_work_grouper():
+    idea_project_name_application_work_grouper = IdeaProjectNameApplicationWorkGrouper()
+    work = [
+        [100, "mistletoe — MerchantLogoService.java [mistletoe.application.main]", "2025-01-01 00:00:00"],
+        [200, "mistletoe — MerchantLogoService.java [mistletoe.application.main]", "2025-01-01 00:00:00"],
+    ]
+    expected_regrouped_work = [
+        [300, "mistletoe"],
+    ]
+    assert expected_regrouped_work == idea_project_name_application_work_grouper.regroup_work_by_tab(work)
--- web-app/work_grouper.py
+++ web-app/work_grouper.py
@@ -105,6 +105,37 @@ class ArcProjectNameApplicationWorkGrouper(ApplicationWorkGrouper):
     def regroup_work_by_tab_and_date_hour(self, work):
         return [[w[0], self.clean_tab_name(w[1]), w[2]] for w in work]
 
+class IdeaProjectNameApplicationWorkGrouper(ApplicationWorkGrouper):
+    def group_key(self):
+        return "project"
+
+    def get_project_name(self, tab):
+        items = tab.split(" — ")
+        if len(items) == 2:
+            return items[0]
+        return tab
+    
+    def regroup_work_by_tab(self, work):
+        work_by_project_name = {}
+        for w in work:
+            work_done_in_secs = w[0]
+            project_name = self.get_project_name(w[1])
+            if project_name not in work_by_project_name:
+                work_by_project_name[project_name] = 0
+            work_by_project_name[project_name] += work_done_in_secs
+        return [[work_by_project_name[project_name], project_name] for project_name in work_by_project_name]
+
+    def regroup_work_by_tab_and_date_hour(self, work):
+        work_by_project_name_and_date_hour = {}
+        for w in work:
+            work_done_in_secs = w[0]
+            project_name = self.get_project_name(w[1])
+            date_hour = w[2]
+            if (project_name, date_hour) not in work_by_project_name_and_date_hour:
+                work_by_project_name_and_date_hour[(project_name, date_hour)] = 0
+            work_by_project_name_and_date_hour[(project_name, date_hour)] += work_done_in_secs
+        return [[work_by_project_name_and_date_hour[(project_name, date_hour)], project_name, date_hour] for project_name, date_hour in work_by_project_name_and_date_hour]
+
 def get_work_grouper(application_name):
     if application_name == "Cursor":
         return CursorProjectNameApplicationWorkGrouper()
@@ -112,4 +143,6 @@ def get_work_grouper(application_name):
         return SlackChannelApplicationWorkGrouper()
     if application_name == "Arc":
         return ArcProjectNameApplicationWorkGrouper()
+    if application_name == "idea":
+        return IdeaProjectNameApplicationWorkGrouper()
     return NoOpApplicationWorkGrouper()
